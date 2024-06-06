3bac1992bf1c16ac9ceae5ec0d6481aa7ee4c043
Rahadi Jalu <jalu.rahadi.rj@gmail.com>
2024-06-06 22:29:25 +0700

    feat: product naming, welcome screen

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 
 import { shareChat } from '@/app/actions'
-import { Button } from '@/components/ui/button'
-import { PromptForm } from '@/components/prompt-form'
 import { ButtonScrollToBottom } from '@/components/button-scroll-to-bottom'
-import { IconShare } from '@/components/ui/icons'
-import { FooterText } from '@/components/footer'
 import { ChatShareDialog } from '@/components/chat-share-dialog'
-import { useAIState, useActions, useUIState } from 'ai/rsc'
+import { FooterText } from '@/components/footer'
+import { PromptForm } from '@/components/prompt-form'
+import { Button } from '@/components/ui/button'
+import { IconShare } from '@/components/ui/icons'
 import type { AI } from '@/lib/chat/actions'
+import { useAIState, useActions, useUIState } from 'ai/rsc'
 import { nanoid } from 'nanoid'
 import { UserMessage } from './stocks/message'
 
@@ -36,24 +36,24 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      heading: 'I want to',
+      subheading: 'check my balance',
+      message: `Show me my account balance`
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      heading: 'I would like to send',
+      subheading: 'Rp. 100.000 to Alice',
+      message: 'Send Rp. 100.000 to Alice with my Savings account'
     },
     {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      heading: 'I would like to pay',
+      subheading: "this month's electricity bill",
+      message: `Pay this month's PLN bill with my Savings account`
     },
     {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      heading: 'I would like to know',
+      subheading: `my recent transaction history`,
+      message: `Show me my transaction history`
     }
   ]
 
